9257f9d3e642b92494685ead1b792e785ad9d092
luojt <244406664@qq.com>
2020-09-01 23:22:02 +0800

    怪物图鉴增加搜索功能

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -98,12 +98,38 @@
       footer-hide
       title="宠物图鉴"
     >
+      <Form ref="formInline" inline>
+        <FormItem>
+          <Select v-model="monsterSearch.skills" filterable multiple placeholder="怪物包含的技能">
+            <Option
+              v-for="(value, key) in skillMap"
+              :key="key"
+              :value="key"
+            >{{key}}</option>
+          </Select>
+        </FormItem>
+        <FormItem>
+          <Select v-model="monsterSearch.rare" multiple placeholder="怪物品质">
+            <Option
+              v-for="(txt, i) in rareType"
+              :key="txt"
+              :value="i"
+            >{{txt}}</option>
+          </Select>
+        </FormItem>
+        <FormItem>
+          <Button type="primary" @click="handlerSearchMonster">搜索</Button>
+        </FormItem>
+      </Form>
+      <h1 v-if="monsterMap.length === 0">没有结果，你得重新搜索</h1>
       <Card
         v-for="monster in monsterMap"
         :key="monster._id"
-        style="margin: 0 8px 8px 0;display:inline-block;width: 250px;vertical-align: top;"
+        style="margin: 0 8px 8px 0;display:inline-block;width: 200px;vertical-align: top;"
         :title="monster.name"
       >
+        <p>等级：{{monster.level || '作者没写'}}</p>
+        <p>副本：{{monster.map || '作者没写'}}</p>
         <p>成长：{{monster.growing_num}}</p>
         <p>品质：{{rareType[monster.type]}}</p>
         <p>躲避资质：{{monster.dodge_zz}}</p>
@@ -113,13 +139,17 @@
         <p>防御资质：{{monster.vit_zz}}</p>
         <p>速度资质：{{monster.speed_zz}}</p>
         <p>躲避资质：{{monster.dodge_zz}}</p>
-        <p>携带技能：</p>
-        <template v-if="monster.skill">
-          <Tag v-for="skill in monster.skill" :key="`${monster._id}${skill._id}`" type="border">
-            {{skill.name}}
-          </Tag>
+        <template v-if="monster.skill && monster.skill.length > 0">
+          <p>携带技能：</p>
+          <Tooltip v-for="skill in monster.skill" :key="`${monster._id}${skill._id}`" word-wrap>
+            <Tag color="primary" type="border">
+              {{skill.name}}
+            </Tag>
+            <div slot="content">
+              {{skill.info}}
+            </div>
+          </Tooltip>
         </template>
-        <p v-else>没技能</p>
       </Card>
     </Modal>
   </div>
@@ -151,7 +181,8 @@ export default {
       skillMap: {},
       showMonsterMap: false,
       monsterMap: [],
-      rareType: ["普通", "稀有", "传说", "PY"]
+      rareType: ["普通", "稀有", "传说", "PY"],
+      monsterSearch: {}
     }
   },
   watch: {
@@ -198,14 +229,12 @@ export default {
 
     const mdata = window.monsterData.data;
     const skillMap = {};
-    console.log(mdata)
     mdata.map(md => {
       md.skill && md.skill.map(sk => {
         skillMap[sk.name] = sk.info;
       });
     });
     this.skillMap = skillMap;
-    this.monsterMap = mdata;
   },
   methods: {
     handleDMflag (flag) {
@@ -290,6 +319,25 @@ export default {
 
       // 更新保存用户
       this.saveStorageUser(email);
+    },
+    handlerSearchMonster () {
+      const mdata = window.monsterData.data;
+      const { skills, rare } = this.monsterSearch;
+      console.log(skills, rare)
+      this.monsterMap = mdata.filter(d => {
+        let flag = true;
+        if (rare && rare.length > 0) {
+          flag = rare.includes(d.type);
+        }
+        if (flag && skills && skills.length > 0) {
+          if (d.skill) {
+            flag = d.skill.some(skl => skills.includes(skl.name))
+          } else {
+            flag = false
+          }
+        }
+        return flag
+      });
     }
   }
 }
